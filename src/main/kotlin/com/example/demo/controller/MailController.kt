12e236bbc8d628045187972d3d601397b71eb15c
YoshikazuHosono <yyhhwork@gmail.com>
2023-01-24 00:12:41 +0900

    add hello world

---
+++ src/main/kotlin/com/example/demo/controller/MailController.kt
@@ -0,0 +1,14 @@
+package com.example.demo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MailController {
+
+    @GetMapping("/mail")
+    fun sendMail(): String {
+        return "Hello hosono ultimate world !!"
+    }
+
+}
